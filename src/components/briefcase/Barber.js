cad7850c89b52b72304dc6be5f1490795ef5ab4e
jcuesta-cidenet-BxIEQV <jcuesta@cidenet.com.co>
2025-02-05 16:28:02 -0500

    Se crea el modulo

---
+++ src/components/briefcase/Barber.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Barber() {
+  return (
+    <div className="row">
+      <div className="col-12">
+        <h5 className="title_job">Barberia</h5>
+        <p>
+          En la pagina Barberia puedes encontrar los servicios, precios, horarios, ubicación, reservas
+          que te puedan ofrecer para una mejor atención, ademas un apartado de entretenimiento.
+        </p>
+      </div>
+      <div className="col-12">
+        <a href="https://jhonatanm30.github.io/barberia/reto-Barberia/page/index.html" className="btn btn-sm" target="_blank">
+          Ir a la Barberia.
+        </a>
+      </div>
+    </div>
+  );
+}
